cf514b8973dd31725b487fbca1c30f5982f50f9a
Jesse Senior <jessesenior@outlook.com>
2025-06-06 18:07:59 +0800

    Add: com.zhihu.android

---
+++ src/apps/com.zhihu.android.ts
@@ -0,0 +1,16 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.zhihu.android',
+  name: '知乎',
+  groups: [
+    {
+      key: 0,
+      name: '局部广告-评论区广告',
+      desc: '点击关闭',
+      fastQuery: true,
+      activityIds: '.comment.ui.activity.CommentListActivity',
+      rules: '[text="广告"] +(n) ViewGroup[clickable=true][desc=""]',
+    },
+  ],
+});
